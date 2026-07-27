d971a75f1c5aad2361754731293734edc2a85a36
Camila S. <cnstos@gmail.com>
2026-07-27 12:18:14 -0300

    Aplicar layout compacto dos níveis no painel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./meta-status.css";
+import "./dashboard-compact.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
